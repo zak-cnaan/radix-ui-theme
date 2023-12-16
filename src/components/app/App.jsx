8c7a082f2161f96741f271f6c7d4a0215222cb46
Zak <izak@moonsite.co.il>
2023-12-17 00:07:24 +0200

    bootstrap normalize

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,13 +1,21 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import reactLogo from '../../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from '../header/header'
+import { Button, Flex, Text } from '@radix-ui/themes'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes :)</Text>
+      <Button>Lets go</Button>
+    </Flex>
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
