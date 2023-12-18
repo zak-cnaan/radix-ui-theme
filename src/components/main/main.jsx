0623663c8b2a7c950fbe6e78a3e5ec1f6e6a8f8e
Zak <izak@moonsite.co.il>
2023-12-18 23:31:37 +0200

    .

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "../app/App";
 // import './index.css'
 import "@radix-ui/themes/styles.css";
+import '../../assets/styles/bs-normalize.css'
 import { Theme } from "@radix-ui/themes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
